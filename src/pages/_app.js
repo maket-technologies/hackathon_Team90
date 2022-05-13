17ad2fac68faaa256e49ae9df4dd78460314d5ec
kruti <kruti.patel0709@gmail.com>
2022-05-13 16:27:07 -0400

    update environmental files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,82 +1,31 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
-import Router from 'next/router';
-import { Toaster } from 'react-hot-toast';
-import { Provider as ReduxProvider } from 'react-redux';
-import nProgress from 'nprogress';
-import { CacheProvider } from '@emotion/react';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { LocalizationProvider } from '@mui/lab';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { RTL } from '../components/rtl';
-import { SettingsButton } from '../components/settings-button';
-import { SplashScreen } from '../components/splash-screen';
-import { SettingsConsumer, SettingsProvider } from '../contexts/settings-context';
-import { AuthConsumer, AuthProvider } from '../contexts/firebase-auth-context';
 import { createEmotionCache } from '../utils/create-emotion-cache';
-import { gtmConfig } from '../config';
-import { gtm } from '../lib/gtm';
-import { store } from '../store';
-import { createTheme } from '../theme';
-import '../i18n';
+import { CacheProvider } from '@emotion/react';
+import { Dashboard } from '../components/dashboard';
 
-Router.events.on('routeChangeStart', nProgress.start);
-Router.events.on('routeChangeError', nProgress.done);
-Router.events.on('routeChangeComplete', nProgress.done);
 
 const clientSideEmotionCache = createEmotionCache();
 
 const App = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-  useEffect(() => {
-    gtm.initialize(gtmConfig);
-  }, []);
-
   return (
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Maket Colaboratory
+          Sustainable
         </title>
         <meta
           name="viewport"
           content="initial-scale=1, width=device-width"
         />
       </Head>
-      <ReduxProvider store={store}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <AuthProvider>
-            <SettingsProvider>
-              <SettingsConsumer>
-                {({ settings }) => (
-                  <ThemeProvider
-                    theme={createTheme({
-                      direction: settings.direction,
-                      responsiveFontSizes: settings.responsiveFontSizes,
-                      mode: settings.theme
-                    })}
-                  >
-                    <RTL direction={settings.direction}>
-                      <CssBaseline />
-                      <Toaster position="top-center" />
-                      <div id="fb-root"></div>
-                      <SettingsButton />
-                      <AuthConsumer>
-                        {(auth) => !auth.isInitialized
-                          ? <SplashScreen />
-                          : <Component {...pageProps} />}
-                      </AuthConsumer>
-                    </RTL>
-                  </ThemeProvider>
-                )}
-              </SettingsConsumer>
-            </SettingsProvider>
-          </AuthProvider>
-        </LocalizationProvider>
-      </ReduxProvider>
+      <h1>Hello</h1>
+      {/* <Component {...pageProps}/>
+      <Dashboard/> */}
     </CacheProvider>
+
   );
 };
 
