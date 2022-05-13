67bb684cbf3680f354d1bdb7f80c280285f823e0
kirti18patel <kirti18patel@yahoo.com>
2022-05-13 17:08:18 -0400

    article db added

---
+++ src/models/Article.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const Type = {
+    academic : 'academic',
+    non_academic : 'non-academic'
+  }
+
+const ArticleSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    tier: {
+      type: String,
+      enum : Type,
+      default: Type.academic
+    },
+    link: {
+        type: String,
+        required: true,
+        trim: true
+    },  
+    image: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    dateCreated: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+module.exports = mongoose.models.Article || mongoose.model('User', ArticleSchema);
