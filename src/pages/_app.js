3109fdb159d33a2fea1373096cea3a372a92b0c4
kirti18patel <kirti18patel@yahoo.com>
2022-05-13 11:45:38 -0400

    initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+
+const App = (props) => {
+
+  return (
+    <>
+      <Head>
+        <title>
+          Sustainable
+        </title>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width"
+        />
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { Divider } from '@mui/material';
+
+const Home = () => {
+
+  return (
+    <>
+      <Head>
+        <title>
+        Sustainable
+        </title>
+      </Head>
+      <main>
+        <Divider />
+      </main>
+    </>
+  );
+};
+
+export default Home;
