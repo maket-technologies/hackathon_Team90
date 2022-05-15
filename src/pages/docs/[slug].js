79b343fba961fc4f3afb4ff1bf165cd7b65b5cd6
kirti18patel <kirti18patel@yahoo.com>
2022-05-15 01:00:55 -0400

    errors removed

--- src/pages/docs/[slug].js
+++ src/pages/docs/[slug].js
@@ -4,7 +4,6 @@ import ErrorPage from 'next/error';
 import { useRouter } from 'next/router';
 import { Container } from '@mui/material';
 import { DocsContent } from '../../components/docs/docs-content';
-import { withDocsLayout } from '../../hocs/with-docs-layout';
 import { gtm } from '../../lib/gtm';
 import { getArticleBySlug, getArticles } from '../../utils/docs';
 
@@ -37,7 +36,7 @@ const Article = (props) => {
   );
 };
 
-export default withDocsLayout(Article);
+export default Article;
 
 export const getStaticProps = ({ params }) => {
   const article = getArticleBySlug(params.slug, ['content', 'slug', 'title']);
